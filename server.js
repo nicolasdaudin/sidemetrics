4caafb017b695e6024f5f064ebe1a958e277e8ab
Nicolas Daudin <nicolas.daudin@gmail.com>
2017-05-18 23:55:06 +0700

    Updated cron time

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.get('/wakeup',function(req,res){
 // CRON TO SEND EMAILS **/
 // every minute: * */1 * * * 
 // every day at 2am : * * 2 * *
-
-var task = cron.schedule('* 35 4 * *', function() {
+// every day ar 4:35am: 4 35 * * *
+var task = cron.schedule('4 35 * * *', function() {
     // CRON STARTED
     var now = moment();
     console.log('CRON BEGIN at',now);
