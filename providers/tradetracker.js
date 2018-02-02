c8e518687b3fdc023f80d96a45f53fcaf0c74955
Nicolas Daudin <nicolas.daudin@gmail.com>
2018-02-02 12:40:16 +0100

    Testing branching 2

--- providers/tradetracker.js
+++ providers/tradetracker.js
@@ -186,7 +186,7 @@ var getEarningsSeveralDays = function (user_id,username,startDay,endDay,after){
 						 dateTo: tradetrackerApiEndDay
 	          		}
 	          	};
-	          	// test branching
+	          	// test branching2
 	          	console.log("[%s] retrieveTradetrackerEarnings - affiliate site id %s",username,affiliateSiteId);
 
 	          	client.getReportAffiliateSite(argsRAS,function getEarningsForSite(err,report){
